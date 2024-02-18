666bba91bac36a787ea3720ef14bc00dd9a9c0d6
avu <anatoliy.us93@gmail.com>
2024-02-18 13:20:43 +0200

    backup 18.02.24v3

--- addresslist_generator.sh
+++ addresslist_generator.sh
@@ -6,12 +6,10 @@ cd radb-tools
 
 python3 ./ip-country-ripe.py RU
 python3 ./ip-country-ripe.py BY
-python3 ./ip-country-ripe.py CN
 
 
-echo /ip/firewall/address-list > address_list_ru_pl_cn.src
-echo remove [find list=ru] >> address_list_ru_pl_cn.src
-cat ip_RU.lst | xargs -I {} echo add address={} list=ru timeout=247d >> address_list_ru_pl_cn.src
-cat ip_BY.lst | xargs -I {} echo add address={} list=ru timeout=247d >> address_list_ru_pl_cn.src
-cat ip_CN.lst | xargs -I {} echo add address={} list=ru timeout=247d >> address_list_ru_pl_cn.src
-mv address_list_ru_pl_cn.src ..
+echo /ip/firewall/address-list > address_list_ru_pl.src
+echo remove [find list=ru] >> address_list_ru_pl.src
+cat ip_RU.lst | xargs -I {} echo add address={} list=ru timeout=247d >> address_list_ru_pl.src
+cat ip_BY.lst | xargs -I {} echo add address={} list=ru timeout=247d >> address_list_ru_pl.src
+mv address_list_ru_pl.src ..
