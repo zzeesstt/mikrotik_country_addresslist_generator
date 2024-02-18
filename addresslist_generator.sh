4b8b7028d0ed5f573c90eca419bab5464b4bf81c
avu <anatoliy.us93@gmail.com>
2024-02-18 14:08:04 +0200

    backup 18.02.24v7

--- addresslist_generator.sh
+++ addresslist_generator.sh
@@ -12,5 +12,5 @@ echo /ip/firewall/address-list > address_list_ru_pl.src
 echo remove [find list=ru] >> address_list_ru_pl.src
 cat ip_RU.lst | xargs -I {} echo add address={} list=ru timeout=247d >> address_list_ru_pl.src
 cat ip_BY.lst | xargs -I {} echo add address={} list=ru timeout=247d >> address_list_ru_pl.src
-echo 'add address=159.138.192.0-159.138.207.255 list=ru timeout=247d comment"Huawai"' >> address_list_ru_pl.src
+echo 'add address=159.138.192.0-159.138.207.255 list=ru timeout=247d comment="Huawai"' >> address_list_ru_pl.src
 mv address_list_ru_pl.src ..
